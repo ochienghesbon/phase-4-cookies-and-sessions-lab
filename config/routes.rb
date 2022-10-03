26fe3b6ff602819543ebd906227d4e44d9e26221
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-10-03 23:34:39 +0300

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+  get  "/sessions", to: "sessions#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
